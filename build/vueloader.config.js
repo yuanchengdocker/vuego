42f55ffc9d92119824f33e0bbb0795065b939360
yuanchengdocker <215854207@qq.com>
2018-04-09 17:41:57 +0800

    webpacklint

--- build/vueloader.config.js
+++ build/vueloader.config.js
@@ -1,11 +1,11 @@
 module.exports = (isDev) => {
-    return {
-        preserveWhitespace:true,
-        extractCSS:!isDev, //针对vue文件的css文件加载入style.css中
-        hotReload:isDev,  //是否开启热重载
-        cssModules:{
-            localIdentName:isDev ? '[path][name]---[local]---[hash:base64:5]' : '[hash:base64:5]' ,  //针对css进行模块化管理，可以重设置name
-            camelCase:true //css命名时候，中间会用-横杆拼接，设置该参数后 名字直接用驼峰获取
-        }
+  return {
+    preserveWhitespace: true,
+    extractCSS: !isDev, // 针对vue文件的css文件加载入style.css中
+    hotReload: isDev, // 是否开启热重载
+    cssModules: {
+      localIdentName: isDev ? '[path][name]---[local]---[hash:base64:5]' : '[hash:base64:5]', // 针对css进行模块化管理，可以重设置name
+      camelCase: true // css命名时候，中间会用-横杆拼接，设置该参数后 名字直接用驼峰获取
     }
+  }
 }
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -7,101 +7,101 @@ const path = require('path')
 
 const isDev = process.env.NODE_ENV === 'development'
 const devServer = {
-    port:'9999',
-    progress:true,
-    overlay:{
-        errors:true
-    },
-    hot:true
-    // open:true
+  port: '9999',
+  progress: true,
+  overlay: {
+    errors: true
+  },
+  hot: true
+  // open:true
 }
 const defaultPlugin = [
-    new webpack.DefinePlugin({
-        'process.env':{
-            NODE_ENV: isDev ? '"development"' : '"production"'
-        }
-    }),
-    new HTMLPlugin()
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new HTMLPlugin()
 ]
 
-const config = isDev ? webpackMerge(baseConfig,{
-    devtool:"#cheap-module-eval-source-map",
-    devServer:devServer,
-    module:{
-        rules:[
-            {
-                test:/\.styl$/,
-                use:[
-                    'vue-style-loader',
-                    {
-                        loader:'css-loader',
-                        options:{
-                            module:true,
-                            localIdentName:isDev ? '[path][name]---[local]---[hash:base64:5]' : '[hash:base64:5]' ,
-                            camelCase:true
-                        }
-                    },
-                    {
-                        loader:'postcss-loader',
-                        options:{
-                            sourceMap:true
-                        }
-                    },
-                    'stylus-loader'
-                ]
+const config = isDev ? webpackMerge(baseConfig, {
+  devtool: '#cheap-module-eval-source-map',
+  devServer: devServer,
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              module: true,
+              localIdentName: isDev ? '[path][name]---[local]---[hash:base64:5]' : '[hash:base64:5]',
+              camelCase: true
             }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
         ]
-    },
-    plugins:defaultPlugin.concat([
-        new webpack.HotModuleReplacementPlugin()
-        // new webpack.NoEmitOnErrorsPlugin()
-    ])
-}) : webpackMerge(baseConfig,{
-    entry:{
-        // vendor:['vue'],
-        bundle:path.join(__dirname,'../src/index.js')
-    },
-    output:{
-        filename:'js/[name].[chunkhash:8].js'
-    },
-    module:{
-        rules:[
+      }
+    ]
+  },
+  plugins: defaultPlugin.concat([
+    new webpack.HotModuleReplacementPlugin()
+    // new webpack.NoEmitOnErrorsPlugin()
+  ])
+}) : webpackMerge(baseConfig, {
+  entry: {
+    // vendor:['vue'],
+    bundle: path.join(__dirname, '../src/index.js')
+  },
+  output: {
+    filename: 'js/[name].[chunkhash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'vue-style-loader',
+          use: [
+            'css-loader',
             {
-                test:/\.styl$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:'vue-style-loader',
-                    use:[
-                        'css-loader',
-                        {
-                            loader:'postcss-loader',
-                            options:{
-                                sourceMap:true
-                            }
-                        },
-                        'stylus-loader'
-                    ]
-                })
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        })
 
-            }
-        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all' // 默认将node_module文件打包到一起
     },
-    optimization:{
-      splitChunks:{
-        chunks:"all" //默认将node_module文件打包到一起
-      },
-      runtimeChunk:true
-    },
-    plugins:defaultPlugin.concat([
-        new ExtractTextPlugin('style.[contentHash:8].css')
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name:'vendor',
-        //     minChunks:Infinity
-        // }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name:'runtime',
-        //     minChunks:Infinity
-        // })
-    ])
+    runtimeChunk: true
+  },
+  plugins: defaultPlugin.concat([
+    new ExtractTextPlugin('style.[contentHash:8].css')
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name:'vendor',
+    //     minChunks:Infinity
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name:'runtime',
+    //     minChunks:Infinity
+    // })
+  ])
 })
 
-module.exports = config;
+module.exports = config
