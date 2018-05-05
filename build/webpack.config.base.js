a266ed7b012ef95ae80ccfa2b644656c2272ac37
袁成 <yuancheng@ACA80066.ipt.aol.com>
2018-05-05 16:01:49 +0800

    geshi

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -4,50 +4,48 @@ const vueloaderConfig = require('./vueloader.config')
 const isDev = process.env.NODE_ENV === 'development'
 
 let config = {
-    mode: process.env.NODE_ENV || "production",
-    entry:{
-        bundle:path.join(__dirname,'../src/index.js')
-    },
-    output:{
-        filename:'js/[name].[hash:8].js',
-        path:path.join(__dirname,'../dist')
-    },
-    module:{
-        rules:[
-            {
-                test:/\.(vue|js|jsx)$/,
-                loader:'eslint-loader',
-                exclude:/node_modules/,
-                enforce:'pre'
-            },
-            {
-                test:/\.vue$/,
-                loader:'vue-loader',
-                options:vueloaderConfig(isDev)
-            },
-            {
-                test:/\.jsx$/,
-                loader:'babel-loader'
-            },{
-                test:/\.js$/,
-                loader:'babel-loader',
-                exclude:/node_modules/
-            },{
-                test:/\.(gif|jpg|jpeg|png|svg)$/,
-                use:[
-                    {
-                        loader:'url-loader',
-                        options:{
-                            limit:1024,
-                            name:'img/[name]-[hash:5].[ext]'
-                        }
-                    }
-                ]
+  mode: process.env.NODE_ENV || 'production',
+  entry: {
+    bundle: path.join(__dirname, '../src/index.js')
+  },
+  output: {
+    filename: 'js/[name].[hash:8].js',
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(vue|js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueloaderConfig(isDev)
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'img/[name]-[hash:5].[ext]'
             }
+          }
         ]
-    }
+      }
+    ]
+  }
 }
 
-
-
-module.exports = config;
+module.exports = config
